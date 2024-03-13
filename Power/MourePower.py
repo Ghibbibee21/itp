e64155ee7973ebf4126c55dacfe69a649c1020ee
Ghibbi Moure <gmoure@berklee.edu>
2024-03-13 15:45:37 -0400

    yummy

---
+++ Power/MourePower.py
@@ -0,0 +1,4 @@
+def print_graph(n):
+def get_power(x, n):
+for i in range(-8, 9):
+print()
--- Pyramid/MourePyramid.py
+++ Pyramid/MourePyramid.py
@@ -1,13 +1,6 @@
 stacks = int(input("How big do ya want this pyramid? "))
-if stacks == 0 or stacks >= 9: 
+if stacks >= 9 or stacks == 0: 
    print ("\nNO!\n") 
-   stacks = int(input("How big do ya want this pyramid? "))
-   if stacks == 0 or stacks >= 9: 
-    print ("\nYou get ONE more try\n") 
-    stacks = int(input("How big do ya want this pyramid? "))
-    if stacks == 0 or stacks >= 9: 
-      print ("\nGo kick some rocks mate :<\n")
-      
 elif stacks >= 1 and stacks <= 8: 
   for i in range(stacks, -1, -1):
     for j in range(stacks):
