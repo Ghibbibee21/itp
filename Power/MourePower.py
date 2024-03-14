346476c9b6e8e77d6a3801795813710cfc8c94bf
Ghibbi Moure <gmoure@berklee.edu>
2024-03-14 10:29:00 -0400

    I did it

--- Power/MourePower.py
+++ Power/MourePower.py
@@ -1,12 +1,15 @@
-def print_graph(n):
-    for i in range (-8, 9):
-        for j in range (0, 9):
-            print('*', end='')
 def get_power(x, n):
         return x ** n
-        for i in range (-8, 9):
-            for j in range (0, 9):
-                print('*', end='')
 
+def print_graph(n):
+    for i in range (-8, 9):
+        x = i
+        y = get_power(x, 2)
+        if y > 0:
+            print('*' * (n * y))
+        elif y == 0:
+             print('' * n)
+        else:
+             print('*')
 print_graph(2)
-get_power(10,2)
+
